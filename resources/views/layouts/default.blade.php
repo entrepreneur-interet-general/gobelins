a17207dc9139086552bc32a0ae5fbd4852ecb283
Ned Baldessin <ned@baldessin.fr>
2018-08-20 18:12:58 +1000

    FE: switch font loading strategy to self-hosted + font-display:swap.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700|Source+Serif+Pro" rel="stylesheet"> 
 </head>
 <body>
     @yield('content')
