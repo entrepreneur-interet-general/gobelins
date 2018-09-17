6d1f1c09fc1cba2669bbf5043ef256b6cf161562
Ned Baldessin <ned@baldessin.fr>
2018-09-17 16:29:19 +0200

    FE: refactor loading of filters, pages, with history management.

--- resources/assets/js/Collection/Collection.jsx
+++ resources/assets/js/Collection/Collection.jsx
@@ -21,80 +21,108 @@ class Collection extends Component {
   constructor(props) {
     super(props);
 
+    const stateFromURL = this.extractStateFromURL();
+
     this.state = {
       hits: [],
-      currentPage: 1,
+      currentPage: stateFromURL.currentPage || 1,
       isLoading: false,
       hasMore: false,
-      filterObj: {}
+      filterObj: stateFromURL.filterObj || {}
     };
+
+    this.cache = {};
+    this.searches = {};
+    this.isLoadingNextPage = false;
+
+    //
 
     this.load = this.load.bind(this);
     this.loadNextPage = this.loadNextPage.bind(this);
     this.handleFilterChange = this.handleFilterChange.bind(this);
     this.buildEndpointUrl = this.buildEndpointUrl.bind(this);
     this.handleLoading = this.handleLoading.bind(this);
+    this.historyPushState = this.historyPushState.bind(this);
+    this.handlePopState = this.handlePopState.bind(this);
+    this.loadFromRemote = this.loadFromRemote.bind(this);
+    this.handleNextPageCallback = this.handleNextPageCallback.bind(this);
+    this.handleFilterChangeCallback = this.handleFilterChangeCallback.bind(
+      this
+    );
+    this.buildSearchParamsFromParams = this.buildSearchParamsFromParams.bind(
+      this
+    );
+    this.buildSearchParamsFromState = this.buildSearchParamsFromState.bind(
+      this
+    );
   }
 
-  load() {
-    if (this.state.isLoading) {
-      return false;
-    } else {
-      this.setState({ isLoading: true }, () => {
-        fetch(this.buildEndpointUrl(), {
-          headers: {
-            Accept: "application/json"
-          }
-        })
-          .then(response => response.json())
-          .then(data => {
-            this.isLoading = false;
-            this.setState(state => ({
-              hits:
-                state.currentPage === 1
-                  ? data.hits
-                  : state.hits.concat(data.hits),
-              hasMore: data.hasMore,
-              isLoading: false
-            }));
-          });
-      });
+  extractStateFromURL() {
+    let urlParams = qs.parse(window.location.search, {
+      ignoreQueryPrefix: true,
+      decoder(value) {
+        if (/^(\d+|\d*\.\d+)$/.test(value)) {
+          return parseFloat(value);
+        }
+
+        let keywords = {
+          true: true,
+          false: false,
+          null: null,
+          undefined: undefined
+        };
+        if (value in keywords) {
+          return keywords[value];
+        }
+
+        return value;
+      }
+    });
+    let out = {};
+    if (urlParams.page) {
+      out.currentPage = urlParams.page;
+      delete urlParams.page;
     }
+    out.filterObj = { ...urlParams };
+    return out;
+  }
+
+  loadFromRemote(searchURL) {
+    return fetch(process.env.MIX_COLLECTION_DSN + searchURL, {
+      headers: {
+        Accept: "application/json"
+      }
+    }).then(response => response.json());
   }
 
   buildEndpointUrl() {
+    return process.env.MIX_COLLECTION_DSN + this.buildSearchParamsFromState();
+  }
+
+  buildSearchParamsFromState() {
     return (
-      process.env.MIX_COLLECTION_DSN +
       "?" +
       qs.stringify(
         { ...this.state.filterObj, page: this.state.currentPage },
-        { arrayFormat: "brackets" }
+        { arrayFormat: "brackets", encodeValuesOnly: true }
       )
     );
   }
 
-  loadNextPage() {
-    // We need a synchronous isLoading property, because
-    // this method is being called outside of the React lifecycle.
-    // Keeping the state.isLoading to handle normal cases.
-    if (this.isLoading === true) {
-      return;
-    }
-    this.isLoading = true;
-    this.setState(state => {
-      return { currentPage: state.currentPage + 1 };
-    });
+  buildSearchParamsFromParams(params) {
+    return (
+      "?" +
+      qs.stringify(params, { arrayFormat: "brackets", encodeValuesOnly: true })
+    );
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      !isEqual(prevState.filterObj, this.state.filterObj) ||
-      prevState.currentPage != this.state.currentPage
-    ) {
-      console.log("filters or current page has changed, calling load()");
-      // TODO: handle history.pushState()
-      this.load();
-    }
+  historyPushState() {
+    let urlBase = window.location.origin + window.location.pathname;
+    window.history.pushState(
+      { ...this.state.filterObj, page: this.state.currentPage },
+      "Recherche",
+      urlBase + this.buildSearchParamsFromState()
+    );
   }
 
   handleFilterChange(filterObj) {
@@ -106,7 +134,140 @@ class Collection extends Component {
   }
 
   componentDidMount() {
-    this.load({}, 1);
+    window.addEventListener("popstate", this.handlePopState);
+    this.firstLoad();
+  }
+  componentWillUnmount() {
+    window.removeEventListener("popstate", this.handlePopState);
+  }
+
+  handlePopState(ev) {
+    // TODO: we'll probably have to integrating a proper router here
+    // once we have the detail-page view.
+    let searchUrl = this.buildSearchParamsFromParams(ev.state);
+    if (this.searches[searchUrl].isLoading === false) {
+      this.setState(state => ({
+        hits: this.searches[searchUrl].data.hits,
+        hasMore: this.searches[searchUrl].data.hasMore,
+        currentPage: ev.state.page,
+        isLoading: false
+      }));
+    }
+  }
+
+  firstLoad() {
+    let searchUrl = this.buildSearchParamsFromState();
+    this.searches[searchUrl] = {
+      isLoading: true,
+      data: {}
+    };
+    this.loadFromRemote(searchUrl).then(data => {
+      this.searches[searchUrl].data = data;
+      this.searches[searchUrl].isLoading = false;
+      this.setState({
+        hits: data.hits,
+        hasMore: data.hasMore,
+        currentPage: 1
+      });
+    });
+  }
+
+  handleNextPageCallback() {
+    // FIXME: this flag isn't stopping the page 2+3 problem…
+    if (this.isLoadingNextPage || !this.state.hasMore) {
+      return;
+    }
+    let pageToLoad = this.state.currentPage + 1;
+    let nextPageUrl = this.buildSearchParamsFromParams({
+      ...this.state.filterObj,
+      page: pageToLoad
+    });
+    if (!this.searches.hasOwnProperty(nextPageUrl)) {
+      this.searches[nextPageUrl] = {
+        isLoading: true,
+        data: {}
+      };
+      this.isLoadingNextPage = true;
+
+      this.loadFromRemote(nextPageUrl).then(data => {
+        this.searches[nextPageUrl].data = data;
+        this.searches[nextPageUrl].isLoading = false;
+        this.setState(
+          state => ({
+            hits:
+              state.currentPage === 1
+                ? data.hits
+                : state.hits.concat(data.hits),
+            hasMore: data.hasMore,
+            currentPage: pageToLoad
+          }),
+          () => {
+            this.historyPushState();
+            this.isLoadingNextPage = false;
+          }
+        );
+      });
+    } else {
+      if (this.searches[nextPageUrl].isLoading === true) {
+        console.log("URL is loading, noop", nextPageUrl);
+        return;
+      } else {
+        console.log("URL is already loaded, noop", nextPageUrl);
+      }
+    }
+  }
+
+  handleFilterChangeCallback(filterObj) {
+    let filters = { ...this.state.filterObj, ...filterObj };
+    let searchUrl = this.buildSearchParamsFromParams({
+      ...filters,
+      page: 1
+    });
+    if (!this.searches.hasOwnProperty(searchUrl)) {
+      this.searches[searchUrl] = {
+        isLoading: true,
+        data: {}
+      };
+      this.setState({ isLoading: true }, () => {
+        this.loadFromRemote(searchUrl).then(data => {
+          this.searches[searchUrl].data = data;
+          this.searches[searchUrl].isLoading = false;
+          this.setState(
+            state => ({
+              hits: data.hits,
+              hasMore: data.hasMore,
+              currentPage: 1,
+              isLoading: false,
+              filterObj: filterObj
+            }),
+            () => {
+              this.historyPushState();
+            }
+          );
+        });
+      });
+    } else {
+      if (this.searches[searchUrl].isLoading === false) {
+        this.setState(
+          state => ({
+            hits: this.searches[searchUrl].data.hits,
+            hasMore: this.searches[searchUrl].data.hasMore,
+            currentPage: 1,
+            isLoading: false
+          }),
+          () => {
+            this.historyPushState();
+          }
+        );
+      }
+    }
+  }
+
+  isLoadingSearch(searchParams) {
+    return (
+      this.searches[searchParams] &&
+      this.searches[searchParams].isLoading === true
+    );
   }
 
   render() {
@@ -117,13 +278,19 @@ class Collection extends Component {
         debounceDelay={100}
       >
         <div className="Collection">
-          <Filters onFilterChange={this.handleFilterChange} />
+          <Filters
+            onFilterChange={this.handleFilterChangeCallback}
+            isLoadingURL={this.isLoadingSearch.bind(
+              this,
+              this.buildSearchParamsFromState()
+            )}
+          />
           <div className="Collection__result">
             {true ? (
               <CollectionGrid
                 hits={this.state.hits}
-                loadMore={this.loadNextPage}
-                hasMore={!this.state.isLoading && this.state.hasMore}
+                loadMore={this.handleNextPageCallback}
+                hasMore={this.state.hasMore}
               />
             ) : (
               <CollectionList hits={this.state.hits} />
