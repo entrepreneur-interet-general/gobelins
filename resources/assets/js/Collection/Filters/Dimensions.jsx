a4f40312b8e7c8097817791100be7489799c46bf
Ned Baldessin <ned@baldessin.fr>
2018-11-08 14:35:41 +0100

    Dimensions filter: handle x-y-z dimensions independently.

--- resources/assets/js/Collection/Filters/Dimensions.jsx
+++ resources/assets/js/Collection/Filters/Dimensions.jsx
@@ -4,15 +4,21 @@ class Dimensions extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      height_or_thickness_lte: props.height_or_thickness_lte || "",
-      height_or_thickness_gte: props.height_or_thickness_gte || "",
-      depth_or_width_lte: props.depth_or_width_lte || "",
-      depth_or_width_gte: props.depth_or_width_gte || "",
-      length_or_diameter_lte: props.length_or_diameter_lte || "",
-      length_or_diameter_gte: props.length_or_diameter_gte || ""
+      height_or_thickness_gte: props.height_or_thickness_gte || 0,
+      height_or_thickness_lte:
+        props.height_or_thickness_lte ||
+        props.dimensions.max_height_or_thickness,
+      depth_or_width_gte: props.depth_or_width_gte || 0,
+      depth_or_width_lte:
+        props.depth_or_width_lte || props.dimensions.max_depth_or_width,
+      length_or_diameter_gte: props.length_or_diameter_gte || 0,
+      length_or_diameter_lte:
+        props.length_or_diameter_lte || props.dimensions.max_length_or_diameter
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
     this.renderFormBlock = this.renderFormBlock.bind(this);
   }
 
@@ -20,25 +26,45 @@ class Dimensions extends Component {
     this.setState({ [type]: event.target.value });
   }
 
-  handleKeyDown(event) {
+  handleKeyDown(type, event) {
     let filterObj = {};
     if (event.keyCode == 13) {
-      [
-        "height_or_thickness_lte",
-        "height_or_thickness_gte",
-        "depth_or_width_lte",
-        "depth_or_width_gte",
-        "length_or_diameter_lte",
-        "length_or_diameter_gte"
-      ].forEach(val => {
-        if (this.state[val]) {
-          filterObj[val] = this.state[val];
-        }
-      });
-      if (Object.keys(filterObj).length > 0) {
-        this.props.onFilterAdd(filterObj);
-      }
+      this.handleUpdate(type);
+      //   [
+      //     "height_or_thickness_lte",
+      //     "height_or_thickness_gte",
+      //     "depth_or_width_lte",
+      //     "depth_or_width_gte",
+      //     "length_or_diameter_lte",
+      //     "length_or_diameter_gte"
+      //   ].forEach(val => {
+      //     if (this.state[val]) {
+      //       filterObj[val] = this.state[val];
+      //     }
+      //   });
+      //   if (Object.keys(filterObj).length > 0) {
+      //     this.props.onFilterAdd(filterObj);
+      //   }
     }
+  }
+
+  handleBlur(type, event) {
+    // In case the user deleted the value, restore default ones.
+    let notEmptyValues = {};
+    if (this.state[type + "_gte"] == "") {
+      notEmptyValues[type + "_gte"] = 0;
+    }
+    if (this.state[type + "_lte"] == "") {
+      notEmptyValues[type + "_lte"] = this.props.dimensions["max_" + type];
+    }
+    this.handleUpdate(type);
+  }
+
+  handleUpdate(type) {
+    let filterObj = {};
+    filterObj[type + "_gte"] = this.state[type + "_gte"];
+    filterObj[type + "_lte"] = this.state[type + "_lte"];
+    this.props.onFilterAdd(filterObj);
   }
 
   renderFormBlock(dimension) {
@@ -53,7 +79,8 @@ class Dimensions extends Component {
               type="text"
               value={this.state[dimension[0] + "_gte"]}
               onChange={this.handleChange.bind(this, dimension[0] + "_gte")}
-              onKeyDown={this.handleKeyDown}
+              onKeyDown={this.handleKeyDown.bind(this, dimension[0])}
+              onBlur={this.handleBlur.bind(this, dimension[0])}
               placeholder="0"
             />
             et
@@ -61,7 +88,8 @@ class Dimensions extends Component {
               type="text"
               value={this.state[dimension[0] + "_lte"]}
               onChange={this.handleChange.bind(this, dimension[0] + "_lte")}
-              onKeyDown={this.handleKeyDown}
+              onKeyDown={this.handleKeyDown.bind(this, dimension[0])}
+              onBlur={this.handleBlur.bind(this, dimension[0])}
               placeholder={this.props.dimensions["max_" + dimension[0]]}
             />
           </div>
