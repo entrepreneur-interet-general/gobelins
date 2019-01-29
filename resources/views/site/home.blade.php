d3468f1615bbdff199db0323382723bb5b8164b5
lauriechap <36261410+lauriechap@users.noreply.github.com>
2019-01-29 14:08:24 +0100

    Content update

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -9,15 +9,14 @@
             </header>
             <div class="BetaWelcome__intro">
                 <p>
-                    Bienvenue sur la nouvelle interface d’exploration du <span class="BetaWelcome__purple">Mobilier national</span>.
+                    Découvrez les œuvres, meubles, et textiles du <span class="BetaWelcome__purple">Mobilier national</span>.
                     Ce site est une <span class="BetaWelcome__salmon">version beta</span> en cours de développement, nous vous souhaitons
                     de belles découvertes en attendant les versions ultérieures.
                 </p>
             </div>
             <aside class="BetaWelcome__aparte BetaWelcome__purple">
                 Le Mobilier national est une institution publique, héritière
-                du garde-Meuble de la couronne, créé en 1604 par Henri IV
-                et réorganisé par Louis XIV.
+                du garde-Meuble de la couronne, créé en 1604 par Henri IV.
                 Cette structure assure la conservation,
                 restauration et création de ses collections.  
             </aside>
