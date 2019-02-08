a0f7f42cbb9a82ae7cb036b9ab25649504c2e17b
Ned Baldessin <ned@baldessin.fr>
2019-02-08 17:35:16 +0100

    Attempt to cure cancer.

--- resources/assets/js/Collection/CollectionGrid.jsx
+++ resources/assets/js/Collection/CollectionGrid.jsx
@@ -36,7 +36,13 @@ class CollectionGrid extends Component {
         <a
           href={`/objet/${datum.inventory_id}`}
           onClick={ev => this.props.onObjectClick(datum, ev)}
-          key={datum["_id"]}
+          key={
+            datum["_id"] +
+            "-" +
+            Math.random()
+              .toString(36)
+              .substring(2, 15)
+          }
           className="Collection__cell"
         >
           {hasImages ? (
