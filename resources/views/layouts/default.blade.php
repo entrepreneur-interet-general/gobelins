fff0c9aef53692c4ea490806bc8b64c73e05ddf3
Ned Baldessin <ned@baldessin.fr>
2019-02-21 16:28:07 +0100

    Add Single Page App page view tracking for Matomo analytics. Refs #126

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <title>Collection du Mobilier national</title>
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
@@ -21,21 +22,39 @@
     <script src="{{ mix('/js/bootstrap.js') }}"></script>
 
     @if (app()->environment('production'))
-    <!-- Matomo -->
-    <script type="text/javascript">
-        var _paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-            var u="//gobelins-stats.eig-apps.org/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '2']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-    </script>
-    <!-- End Matomo Code -->
+        <!-- Matomo -->
+        <script type="text/javascript">
+            var _paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+                var u="//gobelins-stats.eig-apps.org/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '2']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+        </script>
+
+        <script>
+            var currentUrl = location.href;
+            window.addEventListener('popstate', function() {
+                _paq.push(['setReferrerUrl', currentUrl]);
+                currentUrl = '' + window.location.href;
+                _paq.push(['setCustomUrl', currentUrl]);
+                _paq.push(['setDocumentTitle', window.document.title]);
+
+                // remove all previously assigned custom variables, requires Matomo (formerly Piwik) 3.0.2
+                // _paq.push(['deleteCustomVariables', 'page']);
+                _paq.push(['setGenerationTimeMs', 0]);
+                _paq.push(['trackPageView']);
+
+                // make Matomo aware of newly added content
+                _paq.push(['enableLinkTracking']);
+            });
+        </script>
+        <!-- End Matomo Code -->
     @endif
 
 </body>
