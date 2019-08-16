a6406cc3a1e7b6cef6305fdb43e2052ab48dbc1f
Ned Baldessin <ned@baldessin.fr>
2019-08-16 12:47:31 +0200

    MySelections : authenticated blank slate

--- resources/assets/js/Selection/MySelections.jsx
+++ resources/assets/js/Selection/MySelections.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Gateway } from "react-gateway";
 import ReactModal2 from "react-modal2";
+import classNames from "classnames";
 
 import Button from "../ui/Button";
 import { useSelections } from "../context/selections-context";
@@ -8,6 +9,8 @@ import { useAuth } from "../context/auth-context";
 import AuthModal from "../Auth/AuthModal";
 import SelectionsList from "./SelectionsList";
 import PlusLarge from "../icons/PlusLarge";
+import Heart from "../icons/Heart";
+import SelectionsBlank from "../icons/SelectionsBlank";
 import Loader from "../Loader";
 
 export default function MySelections(props) {
@@ -32,7 +35,6 @@ function MySelectionsHeader(props) {
   const authContext = useAuth();
 
   function handleLogout() {
-    console.log("handleLogout");
     const tok = document
       .querySelector("meta[name=csrf-token]")
       .getAttribute("content");
@@ -48,7 +50,7 @@ function MySelectionsHeader(props) {
   }
 
   return (
-    <hgroup className="MySelections__header">
+    <hgroup className={classNames("MySelections__header", props.className)}>
       <h1>Selections de {authContext.data.user.name}</h1>
       <div className="MySelections__header-buttons">
         <Button
@@ -87,14 +89,36 @@ function MySelectionsList(props) {
     <div className="MySelections">
       {selectionsContext.loading ? (
         <Loader />
-      ) : (
+      ) : selectionsContext.mySelections.length ? (
         <ul className="SelectionsList">
           <SelectionsList
             selections={selectionsContext.mySelections}
             rightHeader={<MySelectionsHeader />}
           />
         </ul>
+      ) : (
+        <UserHasNoSelections />
       )}
+    </div>
+  );
+}
+
+function UserHasNoSelections(props) {
+  return (
+    <div className="SelectionsListBlankSlate">
+      <div className="SelectionsListBlankSlate__top-left">
+        <button className="SelectionsListBlankSlate__new">
+          Créer votre première sélection
+        </button>
+      </div>
+      <MySelectionsHeader className="SelectionsListBlankSlate__header" />
+      <div className="SelectionsListBlankSlate__bottom-left">
+        <button className="SelectionsListBlankSlate__label-heart">
+          ou sauvegardez des objets en cliquant sur les
+          <Heart />
+        </button>
+      </div>
+      <SelectionsBlank className="SelectionsListBlankSlate__illu" />
     </div>
   );
 }
@@ -137,16 +161,17 @@ function NotAuthenticated(props) {
             </div>
           </div>
         </div>
-        <div className="MySelections__auth-panel">
+
+        <h1 className="MySelections__auth-panel-title">
           Identifiez-vous pour consulter ou créer vos sélections d’objets
-          <div className="MySelections__auth-panel-buttons">
-            <Button onClick={handleRegisterClick} icon="arrow">
-              créer un compte
-            </Button>
-            <Button onClick={handleLoginClick} icon="arrow">
-              se connecter
-            </Button>
-          </div>
+        </h1>
+        <div className="MySelections__auth-panel-buttons">
+          <Button onClick={handleRegisterClick} icon="arrow">
+            créer un compte
+          </Button>
+          <Button onClick={handleLoginClick} icon="arrow">
+            se connecter
+          </Button>
         </div>
       </div>
       {authModalOpen && (
