e1b2c8401165e73e5d891b50c412b1f0da2c0a19
Ned Baldessin <ned@baldessin.fr>
2019-08-25 16:12:35 +0200

    Fix selection detail blank slate (if not mine).

--- resources/assets/js/icons/SelectionsBlank.jsx
+++ resources/assets/js/icons/SelectionsBlank.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const SelectionsBlank = props => (
   <svg width={376} height={248} viewBox="0 0 376 248" fill="none" {...props}>
-    <path stroke="#fff" d="M1.521 19.023h374.283" />
-    <ellipse cx={11.152} cy={10.455} rx={2.96} ry={2.997} fill="#fff" />
-    <ellipse cx={22.007} cy={10.455} rx={2.96} ry={2.997} fill="#fff" />
-    <ellipse cx={32.862} cy={10.455} rx={2.96} ry={2.997} fill="#fff" />
-    <path stroke="#fff" d="M.5.5h374.804v247H.5z" />
+    <path stroke="currentColor" d="M1.521 19.023h374.283" />
+    <ellipse cx={11.152} cy={10.455} rx={2.96} ry={2.997} fill="currentColor" />
+    <ellipse cx={22.007} cy={10.455} rx={2.96} ry={2.997} fill="currentColor" />
+    <ellipse cx={32.862} cy={10.455} rx={2.96} ry={2.997} fill="currentColor" />
+    <path stroke="currentColor" d="M.5.5h374.804v247H.5z" />
     <path
-      fill="#fff"
+      fill="currentColor"
       fillOpacity={0.1}
       d="M83.885 19.014h79.411v104.017H83.885zM181.056 46.211h79.117v137.093h-79.117zM181.056 218.732h79.117v27.727h-79.117zM278.498 19.014h79.411V138.69h-79.411zM278.43 177.143h79.117v69.317H278.43zM83.68 157.118h79.117v89.342H83.68z"
     />
