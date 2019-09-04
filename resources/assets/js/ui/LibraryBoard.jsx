5b10960deb86853f1aa2346a16e75339ffe32104
Ned Baldessin <ned@baldessin.fr>
2019-09-04 16:37:15 +0200

    Update LibraryBoard with input fields.

--- resources/assets/js/ui/LibraryBoard.jsx
+++ resources/assets/js/ui/LibraryBoard.jsx
@@ -8,25 +8,99 @@ import Switch from "./Switch";
 export default function LibraryBoard(props) {
   return (
     <div className="LibraryBoard">
-      <h1>Buttons</h1>
+      <h1>Boutons</h1>
       <div className="LibraryBoard__zone">
-        <div className="LibraryBoard__item">
+        <div className="LibraryBoard__item is-format-m">
           <div className="LibraryBoard__legend">
-            Attributes: <tt>popOver="première visite"</tt>
+            Attributs: <tt>icon="arrow"</tt>
           </div>
-          <div className="LibraryBoard__display is-format-s is-dark">
+          <div className="LibraryBoard__display">
+            <Button icon="arrow">créer un compte</Button>
+          </div>
+        </div>
+        <div className="LibraryBoard__item is-format-m">
+          <div className="LibraryBoard__legend">
+            Attributs: <tt>popOver="première visite"</tt>
+          </div>
+          <div className="LibraryBoard__display">
             <Button popOver="première visite">créer un compte</Button>
           </div>
         </div>
-        <div className="LibraryBoard__item">
+        <div className="LibraryBoard__item is-format-m">
           <div className="LibraryBoard__legend">
-            Attributes: <tt>popOver="déjà inscrit"</tt>{" "}
+            Attributs: <tt>popOver="déjà inscrit"</tt>{" "}
             <tt>popOverPlacement="bottom"</tt>
           </div>
-          <div className="LibraryBoard__display is-format-s is-dark">
+          <div className="LibraryBoard__display">
             <Button popOver="première visite" popOverPlacement="bottom">
               se connecter
             </Button>
+          </div>
+        </div>
+
+        <div className="LibraryBoard__item is-format-m">
+          <div className="LibraryBoard__legend">
+            Attributs: <tt>dark</tt> <tt>small</tt>
+          </div>
+          <div className="LibraryBoard__display">
+            <Button dark small>
+              modifier
+            </Button>
+          </div>
+        </div>
+
+        <div className="LibraryBoard__item is-format-m">
+          <div className="LibraryBoard__legend">
+            Attributs: <tt>dark</tt> <tt>small</tt> <tt>icon="trashcan"</tt>
+          </div>
+          <div className="LibraryBoard__display">
+            <Button dark small icon="trashcan">
+              supprimer
+            </Button>
+          </div>
+        </div>
+
+        <div className="LibraryBoard__item is-format-m">
+          <div className="LibraryBoard__legend">
+            Attributs: <tt>dark</tt> <tt>small</tt> <tt>round</tt>
+            <tt>icon="trashcan|gear|plus|pencil|cross…"</tt>
+          </div>
+          <div className="LibraryBoard__display">
+            <Button dark small round icon="trashcan" />
+            <Button dark small round icon="gear" />
+            <Button dark small round icon="plus" />
+            <Button dark small round icon="pencil" />
+            <Button dark small round icon="cross" />
+          </div>
+        </div>
+      </div>
+
+      <h1>Champs texte</h1>
+      <div className="LibraryBoard__zone">
+        <div className="LibraryBoard__item is-format-l">
+          <div className="LibraryBoard__legend">
+            Attributs: <tt>label="E-mail"</tt>
+          </div>
+          <div className="LibraryBoard__display">
+            <InputField label="E-mail" name="foo" />
+          </div>
+        </div>
+
+        <div className="LibraryBoard__item is-format-l">
+          <div className="LibraryBoard__legend">
+            Attributs: <tt>label="E-mail"</tt> <tt>type="password"</tt>
+          </div>
+          <div className="LibraryBoard__display">
+            <InputField label="Mot de passe" type="password" name="foo" />
+          </div>
+        </div>
+
+        <div className="LibraryBoard__item is-format-l">
+          <div className="LibraryBoard__legend">
+            Attributs: <tt>label="Intitulé"</tt> <tt>withSubmit</tt>
+          </div>
+          <div className="LibraryBoard__display">
+            <InputField label="Intitulé" withSubmit />
           </div>
         </div>
       </div>
