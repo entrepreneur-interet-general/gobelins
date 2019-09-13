5a4d110810ad2fdc251f98513838bd28ddca7b9f
Ned Baldessin <ned@baldessin.fr>
2019-09-13 15:07:23 +0200

    QA: remove plural form when single result, on search result count. Closes #202

--- resources/assets/js/Collection/ResultCount.jsx
+++ resources/assets/js/Collection/ResultCount.jsx
@@ -3,25 +3,39 @@ import classNames from "classnames";
 
 import ArrowDown from "../icons/ArrowDown";
 
-const ResultCount = props => (
-  <div className={classNames("ResultCount", props.className)}>
-    <div
-      className={`ResultCount__count ${props.totalHits > 0 ? "as-button" : ""}`}
-    >
-      <ArrowDown />
-      {props.totalHits || " "}
-      {props.totalHits > 0 ? " résultats" : "Aucun résultat"}
-    </div>
-    {props.totalHits === 0 ? (
-      <button
-        type="button"
-        className="ResultCount__reset"
-        onClick={props.onFilterRemoveAll}
+const ResultCount = props => {
+  let label;
+  switch (props.totalHits) {
+    case 0:
+      label = "Aucun résultat";
+      break;
+    case 1:
+      label = "1 résultat";
+      break;
+    default:
+      label = `${props.totalHits} résultats`;
+      break;
+  }
+
+  return (
+    <div className={classNames("ResultCount", props.className)}>
+      <div
+        className={`ResultCount__count ${props.totalHits > 0 && "as-button"}`}
       >
-        Réinitialiser la recherche
-      </button>
-    ) : null}
-  </div>
-);
+        <ArrowDown />
+        {label}
+      </div>
+      {props.totalHits === 0 ? (
+        <button
+          type="button"
+          className="ResultCount__reset"
+          onClick={props.onFilterRemoveAll}
+        >
+          Réinitialiser la recherche
+        </button>
+      ) : null}
+    </div>
+  );
+};
 
 export default ResultCount;
