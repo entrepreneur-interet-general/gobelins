a4acebb5fb6d35a3407d50025950ca2c6b7da7a4
Ned Baldessin <ned@baldessin.fr>
2019-11-07 09:59:28 +0100

    Selection detail: speedup loading data by sourcing from ES, not DB.

--- app/Http/Resources/Selection.php
+++ app/Http/Resources/Selection.php
@@ -6,9 +6,18 @@ use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
 
 use App\Http\Resources\User as UserResource;
+use Elasticsearch\ClientBuilder;
 
 class Selection extends JsonResource
 {
+    private $client;
+
+    public function __construct($r)
+    {
+        parent::__construct($r);
+        $this->client = ClientBuilder::create()->build();
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -18,6 +27,19 @@ class Selection extends JsonResource
     public function toArray($request)
     {
         $canUpdateThis = $request->user('api') && $request->user('api')->can('update', $this->resource);
+
+
+        $product_ids = $this->resource->products()->allRelatedIds();
+        if (sizeof($product_ids)) {
+            $es = $this->client->mget([
+                "index" => "gobelins_search",
+                "type" => "products",
+                "body" => ["ids" => $this->resource->products()->allRelatedIds()]
+            ]);
+            $products = collect($es['docs'])->pluck('_source')->all();
+        } else {
+            $products = [];
+        }
         
         return [
             'id' => $this->id,
@@ -36,17 +58,19 @@ class Selection extends JsonResource
             $this->mergeWhen($canUpdateThis, [
                 'invitations' => $this->invitations,
             ]),
-            'products' => $this->products->map(function ($p) {
-                return $p->toSearchableArray();
-                // return [
-                //     'title_or_designation' => $p->title_or_designation,
-                //     'denomination' => $p->denomination,
-                //     'inventory_id' => $p->inventory_id,
-                //     'product_types' => $p->searchableProductTypes,
-                //     'authors' => $p->searchableAuthors,
-                //     'images' => $p->searchableImages,
-                // ];
-            })->all()
+            // 'products' => $this->products->map(function ($p) {
+            //     // return $p->toSearchableArray();
+            //     return [
+            //         'title_or_designation' => $p->title_or_designation,
+            //         'denomination' => $p->denomination,
+            //         'inventory_id' => $p->inventory_id,
+            //         'product_types' => $p->searchableProductTypes,
+            //         'authors' => $p->searchableAuthors,
+            //         'images' => $p->searchableImages,
+            //         'partiallyLoaded' => true,
+            //     ];
+            // })->all()
+            'products' => $products,
         ];
     }
 }
