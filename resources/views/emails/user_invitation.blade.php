0740deab5365a4b1151eb2fa7e89e284b29ec21b
Laurie Chapotte <36261410+lauriechap@users.noreply.github.com>
2019-12-04 14:48:23 +0100

    corrections layout

--- resources/views/emails/user_invitation.blade.php
+++ resources/views/emails/user_invitation.blade.php
@@ -44,8 +44,8 @@ width: 100% !important; height: 100%; margin: 0; line-height: 1.4; background-co
                     <td class="content-cell" style="box-sizing: border-box; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; padding: 35px 35px 0px 35px; word-break: break-word;">
                       <a href="http://collection.mobiliernational.culture.gouv.fr"><img src="{{ $message->embed(public_path('images/emails/collections-mobilier-national-mnlab@2x.jpg')) }}" alt="Collection du Mobilier national" width="100%" style= "padding-bottom: 60px"/></a>
                       
-                      <h1 style="box-sizing: border-box; color: #2F3133; font-family:'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 18px; font-weight: normal; margin-top: 0;" align="left">Bonjour, {{ $name }} !</h1>
-                      <p style="box-sizing: border-box; color: #2F3133; font-family:'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 18px; line-height: 1.5em; margin-top: 0;" align="left">{{ $invite_sender_name }} vous invite à collaborer à sa sélection de meubles, œuvres et textiles du Mobilier national. Commencez la collaboration en cliquant ci-dessous :</p>
+                      <h1 style="box-sizing: border-box; color: #2F3133; font-family:'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 18px; font-weight: normal; margin-top: 0;margin-bottom :0px;" align="left">Bonjour, {{ $name }} !</h1>
+                      <p style="box-sizing: border-box; color: #2F3133; font-family:'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 18px; line-height: 1.5em; margin-top: 3px; padding-top :0px;" align="left">{{ $invite_sender_name }} vous invite à collaborer à sa sélection de meubles, œuvres et textiles du Mobilier national. Commencez la collaboration en cliquant ci-dessous :</p>
                       
                       <table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0" style="box-sizing: border-box; font-family:'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; margin: 30px auto; padding: 0; text-align: center; width: 100%;">
                         <tr>
@@ -57,7 +57,7 @@ width: 100% !important; height: 100%; margin: 0; line-height: 1.4; background-co
                                   <table border="0" cellspacing="0" cellpadding="0" style="box-sizing: border-box; font-family:'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif;">
                                     <tr>
                                       <td style="box-sizing: border-box; font-family:'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; word-break: break-word;">
-                                        <a href="{{ $action_url }}" class="button button--" target="_blank" style="-webkit-text-size-adjust: none; background: #ff4747; border-color: #ff4747; border-radius: 3px; border-style: solid; border-width: 10px 18px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); box-sizing: border-box; color: #FFF; display: inline-block; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; text-decoration: none;">participez</a>
+                                        <a href="{{ $action_url }}" class="button button--" target="_blank" style="-webkit-text-size-adjust: none; background: #1B29B6; border-color: #1B29B6; border-radius: 3px; border-style: solid; border-width: 12px ;padding-right:5px ; padding-left:5px; margin-top:10px; font-size : 18px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); box-sizing: border-box; color: #FFF; display: inline-block; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; text-decoration: none;">participez</a>
                                       </td>
                                     </tr>
                                   </table>
@@ -68,7 +68,7 @@ width: 100% !important; height: 100%; margin: 0; line-height: 1.4; background-co
                         </tr>
                       </table>
                       
-                     <p style="box-sizing: border-box; color: #2F3133; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 18px; line-height: 1.5em; margin-top: 0; margin-bottom: 20px; " align="left">Bonne collaboration ! Si vous avez des questions, n'hésitez pas à nous envoyer un <a href="mailto:{{ $support_email }}" style="box-sizing: border-box; color: #ff4747; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif;">message</a>. 
+                     <p style="box-sizing: border-box; color: #2F3133; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif; font-size: 18px; line-height: 1.5em; padding-top: 10px; margin-top: 0px; margin-bottom: 20px; " align="left">Bonne collaboration ! Si vous avez des questions, n'hésitez pas à nous envoyer un <a href="mailto:{{ $support_email }}" style="box-sizing: border-box; color: #1B29B6; font-family: 'Helvetica Neue',Helvetica,Arial,Verdana,sans-serif;">message</a>. 
                       
                       
                       
