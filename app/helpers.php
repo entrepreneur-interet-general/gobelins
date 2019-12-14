fe01323ca5462772b0d7fbcbe3737df7009ff258
Ned Baldessin <ned@baldessin.fr>
2019-12-14 01:39:29 +0100

    Upgrade Laravel framework from 5.8 to 6.4.1

--- app/helpers.php
+++ app/helpers.php
@@ -7,3 +7,20 @@ if (! function_exists('encodeURIComponent')) {
         return strtr(rawurlencode($str), $revert);
     }
 }
+
+// Ripped from https://github.com/folkloreinc/laravel-image/blob/v1.1/src/Folklore/Image/helpers.php
+
+if (! function_exists('image_url')) {
+    /**
+     * Generate an image url
+     *
+     * @param  string     $src
+     * @param  string  $message
+     * @param  array   $headers
+     * @return string $url
+     */
+    function image_url($src, $width = null, $height = null, $options = [])
+    {
+        return app('image')->url($src, $width, $height, $options);
+    }
+}
