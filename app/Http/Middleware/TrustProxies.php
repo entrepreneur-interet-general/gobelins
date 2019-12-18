200e0fc236c422ba923d2a6ddca0f39efba4ec8f
Ned Baldessin <ned@baldessin.fr>
2019-12-18 10:00:35 +0100

    Add support for trusted proxies, only for production env.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -9,10 +9,12 @@ class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
+     * If none are provided here, the list provided
+     * in config/trustedproxy.php will be used.
      *
      * @var array
      */
-    protected $proxies = '143.126.211.16';
+    protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
