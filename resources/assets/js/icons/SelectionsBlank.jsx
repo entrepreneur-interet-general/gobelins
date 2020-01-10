ee38428cac92c640172f31ed3c92067beaa2e6f1
Ned Baldessin <ned@baldessin.fr>
2020-01-10 16:05:52 +0100

    Animation on selections blank slate illustration. Refs #239

--- resources/assets/js/icons/SelectionsBlank.jsx
+++ resources/assets/js/icons/SelectionsBlank.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const SelectionsBlank = props => (
-  <svg width={376} height={248} viewBox="0 0 376 248" fill="none" {...props}>
+  <svg
+    width={376}
+    height={248}
+    viewBox="0 0 376 248"
+    fill="none"
+    className="SelectionsBlankIcon"
+    {...props}
+  >
     <path stroke="currentColor" d="M1.521 19.023h374.283" />
     <ellipse cx={11.152} cy={10.455} rx={2.96} ry={2.997} fill="currentColor" />
     <ellipse cx={22.007} cy={10.455} rx={2.96} ry={2.997} fill="currentColor" />
@@ -15,14 +22,17 @@ const SelectionsBlank = props => (
     <path
       d="M150.433 115.202c-9.777-5.337-2.785-12.286 0-7.144 2.786-5.142 9.778 1.835 0 7.144z"
       fill="#F24747"
+      className="SelectionsBlankIcon__heart-1"
     />
     <path
       d="M247.74 175.599c-9.778-5.337-2.786-12.286 0-7.144 2.785-5.142 9.777 1.835 0 7.144z"
       fill="#141414"
+      className="SelectionsBlankIcon__heart-2"
     />
     <path
       d="M343.928 128.623c-9.778-5.337-2.786-12.286 0-7.143 2.785-5.143 9.777 1.834 0 7.143z"
       fill="#131313"
+      className="SelectionsBlankIcon__heart-3"
     />
   </svg>
 );
