a9ad67ba0e08c206dd23e5d2db630f440d4515ff
Ned Baldessin <ned@baldessin.fr>
2020-02-24 01:25:52 +0100

    Sitemaps: ensure HTTPS, and proper paths.

--- app/Console/Commands/GenerateSitemaps.php
+++ app/Console/Commands/GenerateSitemaps.php
@@ -43,6 +43,8 @@ class GenerateSitemaps extends Command
         $sitemap = \App::make('sitemap');
         $counter = 0;
         $sitemapCounter = 0;
+        $sitemaps_path = public_path().DIRECTORY_SEPARATOR.'sitemaps';
+
 
         // Add selections dynamic sitemap.
         $sitemap->addSitemap(secure_url(route('selections_sitemap', [], false)));
@@ -51,13 +53,13 @@ class GenerateSitemaps extends Command
             ->published()
             ->orderBy('id', 'asc')
             ->take(1000)
-            ->chunk(10000, function ($products) use (&$sitemap, &$counter, &$sitemapCounter) {
+            ->chunk(10000, function ($products) use (&$sitemap, &$counter, &$sitemapCounter, &$sitemaps_path) {
                 foreach ($products as $p) {
                     if ($counter == 10000) {
                         // generate new sitemap file
-                        $sitemap->store('xml', 'sitemap-' . $sitemapCounter);
+                        $sitemap->store('xml', 'sitemap-' . $sitemapCounter, $sitemaps_path);
                         // add the file to the sitemaps array
-                        $sitemap->addSitemap(secure_url('sitemap-' . $sitemapCounter . '.xml'));
+                        $sitemap->addSitemap(secure_url('sitemaps/sitemap-' . $sitemapCounter . '.xml'));
                         // reset items array (clear memory)
                         $sitemap->model->resetItems();
                         // reset the counter
@@ -86,7 +88,7 @@ class GenerateSitemaps extends Command
                     // $p->freq = 'weekly';
                     // $p->priority = '0.5';
                     $p->timestamp = $p->updated_at;
-                    $p->permalink = route('product', $p->inventory_id);
+                    $p->permalink = secure_url(route('product', $p->inventory_id, false));
                     $sitemap->add($p->permalink, $p->timestamp, null, null, $images);
                     // count number of elements
                     $counter++;
@@ -96,9 +98,10 @@ class GenerateSitemaps extends Command
         // you need to check for unused items
         if (!empty($sitemap->model->getItems())) {
             // generate sitemap with last items
-            $sitemap->store('xml', 'sitemap-' . $sitemapCounter);
+            $sitemap->store('xml', 'sitemap-' . $sitemapCounter, $sitemaps_path);
             // add sitemap to sitemaps array
-            $sitemap->addSitemap(secure_url('sitemap-' . $sitemapCounter . '.xml'));
+
+            $sitemap->addSitemap(secure_url('sitemaps/sitemap-' . $sitemapCounter . '.xml'));
             // reset items array
             $sitemap->model->resetItems();
         }
@@ -106,8 +109,7 @@ class GenerateSitemaps extends Command
         // generate new sitemapindex that will contain all generated sitemaps above
         // Sitemaps are stored in a directory shared between deploys
         // (/var/www/gobelins/shared/sitemaps), see gobelins-devops repo.
-        $path = public_path().DIRECTORY_SEPARATOR.'sitemaps';
-        $sitemap->store('sitemapindex', 'sitemap', $path);
+        $sitemap->store('sitemapindex', 'sitemap', $sitemaps_path);
 
         $end = microtime(true);
         $this->info("Took ".($end - $start) ." seconds to complete\n");
