fb236cccac91fcb3b129b4b9540d4b69cf87fb78
Ned Baldessin <ned@baldessin.fr>
2020-09-09 23:31:39 +0200

    Doh

--- app/Console/Commands/RenameImagesFromOrig.php
+++ app/Console/Commands/RenameImagesFromOrig.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class RenameImages extends Command
+class RenameImagesFromOrig extends Command
 {
     /**
      * The name and signature of the console command.
