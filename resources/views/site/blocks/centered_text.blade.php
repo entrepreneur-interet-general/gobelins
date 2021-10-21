54ba1354174c0d8fb7bce723d4b73ed79410c435
Ned Baldessin <ned@baldessin.fr>
2021-10-22 00:06:45 +0200

    Remove centered_text block, not needed anymore, replaced by 'Texte en exergue', properly this time.

--- resources/views/site/blocks/centered_text.blade.php
+++ resources/views/site/blocks/centered_text.blade.php
@@ -1,3 +1,6 @@
+{{-- 
+    This block is deprecated. We are leaving it here for backwards compatibility with legacy content already created.
+ --}}
 <div class="CenteredText @if($block->input('dark_bg')) on-dark-bg @endif @if($block->input('is_quote')) CenteredText--is_quote @endif">
 
     <div class="has-decorative-bg"></div>
